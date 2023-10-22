e4d2fc2bba22dc16e7e6be0b2e5b49f340c95cf0
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-10-22 15:22:29 -0300

    Ultima Modificacion del Codigo.sh
    Leer un archivo de texto y mostrar una lista de las 10 palabras más repetidas con su respectiva frecuencia y las 5 letras más repetidas con su frecuencia correspondiente.
    
    
    
    by: iTz_DiLAN

--- Trabajo_SistemasOperativos/Codigo.sh
+++ Trabajo_SistemasOperativos/Codigo.sh
@@ -3,6 +3,16 @@
 # Declaracion de las funciones.
 
 # Funcion que verifica si existe el archivo o no.
+Presentacion(){
+  echo "*************************************** Datos del Grupo *************************************************************************"
+  echo "Integrante 1: Jeison Salas Colma --> Rut: 15.306.099-1"
+  echo "Integrante 2: Roberto Barros Gutiérrez --> Rut: 20.759.294-3"
+
+  echo " "
+  echo "Trabajo de BASH - Sistemas Operativos"
+  echo "Profesor: Felipe Tirado"
+  echo "---------------------------------------------------------------------------------------------------------------------------------"
+}
 VerificarSiExisteArchivo(){
   archivo=$1 # Archivo que recibre por parametro la funcion.
 
@@ -23,6 +33,7 @@ VerificarSiExisteArchivo(){
 
 # Funcion que mostrara el contenido de el archivo.
 MostrarContenidoArchivo(){
+  echo "-------------------------------------------------------------------------------------------------------------------------------"
   archivo=$1 # Archivo que recibe por parametro la funcion.
 
   while IFS= read -r linea
@@ -30,6 +41,7 @@ MostrarContenidoArchivo(){
     echo "$linea"
   done < "$archivo"
 
+  echo "-------------------------------------------------------------------------------------------------------------------------------"
   echo " "
 }
 
@@ -41,12 +53,15 @@ GenerarCopiaDelArchivo(){
   archivoCopia1="CopiaArchivo1.txt"
   archivoFinal="TextoFinal.txt"
 
+  nombreArchivo=$(basename "$archivo")
+  echo "Nombre del archivo es $nombreArchivo"
+
   cp "$archivo" "$archivoCopia"
   # Se llama a la funcion VerificarSiExisteArchivo, para comprobar que se ha creado el archivo copia.
   VerificarSiExisteArchivo "$archivoCopia"
   
   # cat MiTexto.txt | tr -d 'áéíóúÁÉÍÓÚa' > CopiaArchivo.txt
-  cat MiTexto.txt | sed 's/á/a/g; s/é/e/g; s/í/i/g; s/ó/o/g; s/ú/u/g;
+  cat $nombreArchivo | sed 's/á/a/g; s/é/e/g; s/í/i/g; s/ó/o/g; s/ú/u/g;
                          s/Á/A/g; s/É/E/g; s/Í/I/g; s/Ó/O/g; s/Ú/U/g;' > CopiaArchivo.txt
 
   cp "$archivoCopia" "$archivoCopia1" 
@@ -62,7 +77,7 @@ GenerarCopiaDelArchivo(){
   VerificarSiExisteArchivo "$archivoCopia"
   VerificarSiExisteArchivo "$archivoCopia1"
   VerificarSiExisteArchivo "$archivoFinal"
-
+ 
   echo " "
   echo "Contenido a Minusculas, sin Acentos y sin signos de Puntuacion"
   #MostrarContenidoArchivo $archivoFinal
@@ -71,21 +86,53 @@ GenerarCopiaDelArchivo(){
 
 ObtenerFrecuenciasPalabras(){
   archivo=$1
-  echo "Desde la ultima funcion"
   MostrarContenidoArchivo $archivo
 
- PalabrasFrecuentes=($(cat "$archivo" | tr -s '[:space:]' '\n' | grep -oE '\w+' | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2}'))
- Frecuencia=($(cat "$archivo" | tr -s '[:space:]' '\n' | grep -oE '\w+' | sort | uniq -c | sort -nr | head -n 5 | awk '{print $1}'))
+  PalabrasFrecuentes=($(cat "$archivo" | tr -s '[:space:]' '\n' | grep -oE '\w+' | sort | uniq -c | sort -nr | head -n 10 | awk '{print $2}'))
+  Frecuencia=($(cat "$archivo" | tr -s '[:space:]' '\n' | grep -oE '\w+' | sort | uniq -c | sort -nr | head -n 10 | awk '{print $1}'))
  
- for ((i=0; i<${#PalabrasFrecuentes[@]}; i++))
- do
-   echo "La palabra: [${PalabrasFrecuentes[i]}] se repite: ${Frecuencia[i]} veces."
- done
+  echo "Las palabras con mas frecuencia del archivo $archivo son: "
+  echo " "
 
+  for ((i=0; i<${#PalabrasFrecuentes[@]}; i++))
+  do
+    echo "La $((i + 1)) palabra [${PalabrasFrecuentes[i]}] se repite: ${Frecuencia[i]} veces."
+  done
+
+  echo " "
+}
+
+ObtenerFrecuenciasLetras(){
+  archivo=$1
+
+  LetrasFrecuentes=($(cat "$archivo" | tr -s '[:space:]' | grep -o . | sort | uniq -c | sort -nr | head -n 6 | awk '{print $2}'))
+  Frecuencia=($(cat "$archivo" | tr -s '[:space:]' | grep -o . | sort | uniq -c | sort -nr | head -n 5 | awk '{print $1}'))
+
+  echo "Las letras con mas frecuencia del archivo $archivo son: "
+  echo " "
+
+  for ((i=0; i<${#LetrasFrecuentes[@]}; i++))
+  do
+    echo "La $((i + 1)) letra [${LetrasFrecuentes[i]}] se repite: ${Frecuencia[i]} veces."
+  done
 }
 
 # Llamada de las funciones.
-VerificarSiExisteArchivo MiTexto.txt
-MostrarContenidoArchivo MiTexto.txt
-GenerarCopiaDelArchivo MiTexto.txt
-ObtenerFrecuenciasPalabras TextoFinal.txt
+Presentacion
+#--------------------------------------------
+nombreDelArchivo="MiTexto.txt"
+VerificarSiExisteArchivo "$nombreDelArchivo"
+contador=$?
+
+if [ "$contador" -eq 1 ]; then
+  echo "Contenido del archivo original: $nombreDelArchivo"
+  MostrarContenidoArchivo "$nombreDelArchivo"
+  GenerarCopiaDelArchivo "$nombreDelArchivo"
+  ObtenerFrecuenciasPalabras TextoFinal.txt
+  ObtenerFrecuenciasLetras TextoFinal.txt
+else
+  echo "El archivo no se encontro."
+fi
+
+echo " "
+echo "Cerrando programa..."
